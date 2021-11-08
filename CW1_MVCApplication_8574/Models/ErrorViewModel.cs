c77b059c7767d2617df48e2aee817222e35778bf
00008574wiut@gmail.com <00008574wiut@gmail.com>
2021-11-09 02:29:21 +0500

    project recreated

---
+++ CW1_MVCApplication_8574/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CW1_MVCApplication_8574.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CW1_MVCApplication_8574/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW1_MVCApplication_8574.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public Category ProductCategory { get; set; }
+    }
+}
